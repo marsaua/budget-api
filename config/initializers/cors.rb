a2bd4a37224c32c4b5cb0b647df601dcf13021b9
marsaua001 <marsaua001@gmail.com>
2026-05-18 15:39:19 +0300

    fix: CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174"
+    origins "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", 'https://abrakadabramarsa.space'
 
     resource "*",
       headers: :any,
